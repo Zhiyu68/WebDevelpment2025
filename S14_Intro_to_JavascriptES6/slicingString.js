18b3b048104db2ef869c7a2ba584bccea293900c
Zhiyu68 <ZhongZ5@cardiff.ac.uk>
2025-04-04 11:37:55 +0100

    completed s14

---
+++ S14_Intro_to_JavascriptES6/slicingString.js
@@ -0,0 +1,8 @@
+function lifeInWeeks(age) {
+  //Write your code here.
+  var leftYears = 90 - age;
+
+  var x = leftYears * 365;
+  var y = leftYears * 52;
+  var z = leftYears * 12;
+}
---
+++ S14_Intro_to_JavascriptES6/stringConcatenation.js
@@ -0,0 +1,12 @@
+var textInput =
+  "gdgdfgusdgdbgdsbgvdsbgglgdbfgdlfbvkbdkvbdflbvdbvhdbvbdgvbdkbgdskbksdbvkdsbkdsbkdsbfjkdsbfjksbfkjsbdfksdbkdsbkgdsbgkbdskbgdshbgkdshbgkhdbgkdshbgksbgdskhbgbvhjfbjdbvhjfbjdfgdjvnvbjbv";
+
+var existingText = textInput.length;
+var leftTextNumber = 140 - existingText;
+console.log(
+  "You have written " +
+    existingText +
+    " characters, you have " +
+    leftTextNumber +
+    " characters left."
+);
